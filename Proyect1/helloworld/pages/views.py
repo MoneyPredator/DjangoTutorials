99dfa8fb4fd098063cdf675f35a4d514de7fee8c
Santiago <zopotamadre290@gmail.com>
2025-03-23 16:34:31 -0500

    Cambios no guardados

--- Proyect1/helloworld/pages/views.py
+++ Proyect1/helloworld/pages/views.py
@@ -12,8 +12,6 @@ from .models import Product
 class HomePageView(TemplateView): 
     template_name = 'pages/home.html' 
  
- 
- 
 class AboutPageView(TemplateView): 
     template_name = 'pages/about.html' 
  
@@ -122,3 +120,48 @@ class ContactPageView(TemplateView):
             "phone": "+123 456 7890",
         })
         return context
+
+class CartView(View): 
+    template_name = 'cart/index.html' 
+     
+    def get(self, request): 
+        # Simulated database for products 
+        products = {} 
+        products[121] = {'name': 'Tv samsung', 'price': '1000'} 
+        products[11] = {'name': 'Iphone', 'price': '2000'} 
+ 
+        # Get cart products from session 
+        cart_products = {} 
+        cart_product_data = request.session.get('cart_product_data', {}) 
+ 
+        for key, product in products.items(): 
+            if str(key) in cart_product_data.keys(): 
+                cart_products[key] = product 
+ 
+        # Prepare data for the view 
+        view_data = { 
+            'title': 'Cart - Online Store', 
+            'subtitle': 'Shopping Cart', 
+            'products': products, 
+            'cart_products': cart_products 
+        } 
+ 
+        return render(request, self.template_name, view_data)
+    
+    def post(self, request, product_id): 
+        # Get cart products from session and add the new product 
+        cart_product_data = request.session.get('cart_product_data', {}) 
+        cart_product_data[product_id] = product_id 
+        request.session['cart_product_data'] = cart_product_data 
+
+        return redirect('cart_index')
+ 
+ 
+class CartRemoveAllView(View): 
+    def post(self, request): 
+        # Remove all products from cart in session 
+        if 'cart_product_data' in request.session: 
+            del request.session['cart_product_data'] 
+ 
+        return redirect('cart_index')
+    
